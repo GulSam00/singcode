d90e121207e4be14eadaa40cf6f2d915ff0d200c
GulSam00 <gulsamcono@gmail.com>
2025-03-21 18:35:19 +0900

    feat : 카카오 auth 연결

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,16 +1,22 @@
 import { redirect } from 'next/navigation';
 import { createClient } from '@/supabase/server';
 import { login, register } from './actions';
+import KakaoLoginButton from './KakaoLoginButton'; // 새로운 클라이언트 컴포넌트
 
 export default async function LoginPage() {
   const supabase = await createClient();
 
   const { data } = await supabase.auth.getUser();
-  // data 있으면 라우팅
+
   if (data) {
     console.log('data : ', data);
-    redirect('/');
+    // redirect('/');
   }
+
+  // const kakao_client_id = process.env.KAKAO_REST_API_KEY;
+  // const kakao_redirect_uri = process.env.KAKAO_REDIRECT_URI;
+
+  // const kakao_url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakao_client_id}&redirect_uri=${kakao_redirect_uri}`;
 
   return (
     <form>
@@ -20,6 +26,7 @@ export default async function LoginPage() {
       <input id="password" name="password" type="password" required />
       <button formAction={login}>Log in</button>
       <button formAction={register}>Sign up</button>
+      <KakaoLoginButton />
     </form>
   );
 }
