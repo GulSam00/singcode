74ae3fbbf1d46289f4936f472772b2d1cbdf8964
GulSam00 <gulsamcono@gmail.com>
2025-04-15 11:04:53 +0900

    fix : env 경로 수정

--- apps/web/app/lib/supabase/api.ts
+++ apps/web/app/lib/supabase/api.ts
@@ -4,28 +4,24 @@ import { type NextApiRequest, type NextApiResponse } from 'next';
 // API client
 
 export default function createClient(req: NextApiRequest, res: NextApiResponse) {
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return Object.keys(req.cookies).map(name => ({
-            name,
-            value: req.cookies[name] || '',
-          }));
-        },
-        setAll(cookiesToSet) {
-          res.setHeader(
-            'Set-Cookie',
-            cookiesToSet.map(({ name, value, options }) =>
-              serializeCookieHeader(name, value, options),
-            ),
-          );
-        },
+  const supabase = createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
+    cookies: {
+      getAll() {
+        return Object.keys(req.cookies).map(name => ({
+          name,
+          value: req.cookies[name] || '',
+        }));
+      },
+      setAll(cookiesToSet) {
+        res.setHeader(
+          'Set-Cookie',
+          cookiesToSet.map(({ name, value, options }) =>
+            serializeCookieHeader(name, value, options),
+          ),
+        );
       },
     },
-  );
+  });
 
   return supabase;
 }
