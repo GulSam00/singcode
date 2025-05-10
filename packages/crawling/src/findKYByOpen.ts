927fee320df8f66d35938445f2cb90b8b3b500f0
GulSam00 <gulsamcono@gmail.com>
2025-05-11 00:29:34 +0900

    fix : 금영 곡 업데이트 방식 변경.

--- packages/crawling/src/findKYByOpen.ts
+++ packages/crawling/src/findKYByOpen.ts
@@ -1,25 +1,78 @@
-import { getSong } from "@repo/open-api";
+import { getSong, getSinger } from "@repo/open-api";
 import { Song } from "./types";
 import { updateKYDB } from "./supabase/updateDB";
+import { getKYNULLDB } from "./supabase/getDB";
+import { logUnknownData } from "./logData";
 
-const getKYByOpen = async (song: Song) => {
+const resultsLog = {
+  success: [] as Song[],
+  failed: [] as { song: Song; error: any }[],
+};
+
+const updateKYByOpen = async (song: Song) => {
   const { title, artist } = song;
-  const response = await getSong({ title, brand: "kumyoung" });
-
+  const trimTitle = title.trim();
   const trimArtist = artist.trim();
+  // console.log(artist, "-", title);
+
+  const response = await getSong({ title: trimTitle, brand: "kumyoung" });
+
+  if (!response || response.length === 0 || !Array.isArray(response)) {
+    resultsLog.failed.push({ song, error: "there is no kumyoung song" });
+    return null;
+  }
+
   // 가수 일치하거나 비슷한지 조회
-  if (response && response.length > 0) {
+  console.log("금영 title 일치 개수 ", response.length, "개");
+
+  // console.log(response);
+  if (response && response.length > 1) {
+    // filter의 includes 만으로는 완벽 비교 불가. chatGPT API를 활용해야 할까...?
     const filteredResponse = response.filter((item) => {
       const artistName = item.singer.trim();
       return artistName.includes(trimArtist);
     });
+    console.log(filteredResponse);
 
     if (filteredResponse.length === 1) {
       const kyNum = filteredResponse[0].no;
-      await updateKYDB({ ...song, num_ky: kyNum });
-      //   return { ...song, num_ky: kyNum };
+      // console.log("filteredResponse kyNum", kyNum);
+      const result = await updateKYDB({ ...song, num_ky: kyNum });
+      if (result) {
+        resultsLog.success.push({ ...song, num_ky: kyNum });
+      } else {
+        resultsLog.failed.push({ song, error: "supabase update failed" });
+      }
+    } else {
+      console.log("필터링 실패");
+    }
+  } else {
+    const kyNum = response[0].no;
+    // console.log("response kyNum", kyNum);
+    const result = await updateKYDB({ ...song, num_ky: kyNum });
+    if (result) {
+      resultsLog.success.push({ ...song, num_ky: kyNum });
+    } else {
+      resultsLog.failed.push({ song, error: "supabase update failed" });
     }
   }
-
-  return null;
 };
+
+const kyNullData = await getKYNULLDB();
+console.log("kyNullData", kyNullData.length);
+
+for (const song of kyNullData) {
+  await updateKYByOpen(song);
+}
+
+// 1차 시도
+// 6079개 업데이트
+
+console.log(`
+    총 ${kyNullData.length}곡 중:
+    - 성공: ${resultsLog.success.length}곡
+    - 실패: ${resultsLog.failed.length}곡
+  `);
+
+logUnknownData(resultsLog.success, "log/findKYByOpenSuccess.txt");
+logUnknownData(resultsLog.failed, "log/findKYByOpenSuccess.txt");
--- packages/crawling/src/logData.ts
+++ packages/crawling/src/logData.ts
@@ -1,8 +1,8 @@
 import fs from "fs";
 import path from "path";
 
-export function logUnknownData<T>(unknownData: T[], filename: string) {
-  if (unknownData.length === 0) return;
+export function logUnknownData<T>(unknownData: T[] | T, filename: string) {
+  if (!unknownData) return;
   const now = new Date();
   const timeString = now.toLocaleString("ko-KR", {
     timeZone: "Asia/Seoul",
@@ -17,11 +17,19 @@ export function logUnknownData<T>(unknownData: T[], filename: string) {
 
   const logPath = path.join(filename);
 
-  // 로그 문자열 생성
-  const logString =
-    `\n[${timeString}]\n` +
-    unknownData.map((item) => JSON.stringify(item)).join("\n") +
-    "\n";
+  if (unknownData instanceof Array) {
+    // 로그 문자열 생성
+    const logString =
+      `\n[${timeString}]\n` +
+      unknownData.map((item) => JSON.stringify(item)).join("\n") +
+      "\n";
 
-  fs.appendFileSync(logPath, logString, "utf-8");
+    fs.appendFileSync(logPath, logString, "utf-8");
+  } else {
+    // 로그 문자열 생성
+    const logString =
+      `\n[${timeString}]\n` + JSON.stringify(unknownData) + "\n";
+
+    fs.appendFileSync(logPath, logString, "utf-8");
+  }
 }
